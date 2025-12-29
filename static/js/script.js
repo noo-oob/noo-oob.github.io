57cd586521b2508662793144dfca3a40b101e0ad
Noob <136036176+noo-oob@users.noreply.github.com>
2025-12-29 21:53:49 +0900

    added background effect and changed things to make it better idk ill improve the background later

--- static/js/script.js
+++ static/js/script.js
@@ -37,3 +37,20 @@ const timeupdate = setInterval(function() {
 
     document.getElementById('time').textContent = "My current timezone is GMT"+timezones+", so it's currently "+ctime+" for me"
 }, 1000);
+
+background = document.querySelector('.background')
+if (background)
+{
+    let time = 0;
+    let animationId;
+    
+    function animatePolygons() {
+        time += 0.0025;
+        animationId = requestAnimationFrame(animatePolygons)
+
+        background.style.setProperty('--blue', 'polygon('+(25+(Math.sin(time*15)*-5))+'% '+(50+(Math.sin(time*10)*5))+'%, '+(50+(Math.sin(time*7)*6))+'% '+(75+(Math.sin(time*8)*7))+'%, '+(75+(Math.sin(time*16)*3))+'% '+(50+(Math.sin(time*4)*-6))+'%, '+(50+(Math.sin(time*-9)*-6))+'% '+(25+(Math.cos(time*15)*-5))+'%)')
+        background.style.setProperty('--purple', 'polygon('+(40+(Math.sin(time*5)*-7))+'% '+(75+(Math.cos(time*6)*7))+'%, '+(60+(Math.cos(time*7)*-8))+'% '+(75+(Math.sin(time*8)*8))+'%, '+(75+(Math.cos(time*9)*-9))+'% '+(50+(Math.sin(time*10)*9))+'%, '+(50+(Math.cos(time*11)*-10))+'% '+(25+(Math.cos(time*12)*10))+'%, '+(25+(Math.sin(time*13)*-11))+'% '+(50+(Math.sin(time*14)*11))+'%)')
+    }
+
+    animatePolygons()
+}
